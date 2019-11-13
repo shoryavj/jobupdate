a92ed9d46aaa4a19f9a73042d4a1870af77d6c04
shoryavj <22168535+shoryavj@users.noreply.github.com>
2019-11-13 18:43:52 +0530

    Create jobupdate.cs

---
+++ jobupdate.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+
+namespace Api.Nomad
+
+{
+     class UpdateChanges
+    {
+
+        static void Main(string[] args)
+        {
+
+            {
+
+                
+                var Content = args[0];
+                var Path = args[1];
+//                Console.WriteLine($"{Path}");
+               var root = ("./WORKDIR/" + Path);
+
+                This text is added only once to the file.
+                if (!File.Exists(root))
+                {
+                    // Create a file to write to.
+                    File.WriteAllText(root, Content);
+                }
+
+
+
+                Open the file to read from.
+                string readText = File.ReadAllText(root);
+               Console.WriteLine(readText);
+            }
+        }
+
+    }
+
+
+}
