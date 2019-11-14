952efab480ae54bbab21d1d431bb9f2f0b89d1f7
shoryavj <22168535+shoryavj@users.noreply.github.com>
2019-11-14 16:53:01 +0530

    Update jobupdate.cs

--- jobupdate.cs
+++ jobupdate.cs
@@ -8,16 +8,18 @@ namespace Api.Nomad
      class UpdateChanges
     {
 
-        static void Main(string[] args)
-        {
 
-            {
+          static void Main(string args)
+        { 
+                string[] jsonArray = JsonConvert.DeserializeObject(args);
 
+                    Console.WriteLine(jsonArray);
+               
                 
-                string Content = args[0];
-                string Path = args[1];
+      //          string Content = args[0];
+        //        string Path = args[1];
 //                Console.WriteLine($"{Path}");
-                string root = ("./../" + Path);
+          //      string root = ("./../" + Path);
                // Console.Writeline(Content) ;
                 //Console.Writeline(Path) ;
                 //Console.Writeline(root) ;
@@ -25,18 +27,15 @@ namespace Api.Nomad
                // This text is added only once to the file.
                
                     // Create a file to write to.
-                    File.WriteAllText(root, Content);
-                
-
-
-
+            //        File.WriteAllText(root, Content);
+             
                 //Open the file to read from.
-                string readText = File.ReadAllText(root);
+              //  string readText = File.ReadAllText(root);
                                //   Thread.Sleep(15000);
 
-               Console.WriteLine(readText);
+               // Console.WriteLine(readText);
             }
-        }
+        
 
     }
 
