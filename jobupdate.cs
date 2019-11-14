5961aeb1b18967b954789e7f13a60fd940873cc7
shoryavj <22168535+shoryavj@users.noreply.github.com>
2019-11-14 10:14:23 +0530

    Update jobupdate.cs

--- jobupdate.cs
+++ jobupdate.cs
@@ -33,6 +33,7 @@ namespace Api.Nomad
                 //Open the file to read from.
                 string readText = File.ReadAllText(root);
                Console.WriteLine(readText);
+                 Thread.Sleep(5000);
             }
         }
 
